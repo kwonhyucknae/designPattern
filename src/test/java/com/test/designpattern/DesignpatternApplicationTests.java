2e2d420bbac5f83d7e7e213581fa01b38e0c7846
hyeoknae.kwon <nhk321@naver.com>
2021-03-16 21:52:27 +0900

    initialize

---
+++ src/test/java/com/test/designpattern/DesignpatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.designpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
